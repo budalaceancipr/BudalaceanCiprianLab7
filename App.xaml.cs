cf2d1ad4908d167c5bb7dcd04708d01d013acd91
Budalacean Ciprian <budalaceancipr@gmail.com>
2025-11-28 17:00:36 +0200

    Lab8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,25 @@
-﻿namespace BudalaceanCiprianLab7
+﻿using System;
+using BudalaceanCiprianLab7.Data;
+using System.IO;
+
+namespace BudalaceanCiprianLab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
